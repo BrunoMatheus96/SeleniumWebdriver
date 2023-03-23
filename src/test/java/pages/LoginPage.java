53fab268accafcd3787a2cae6098af3d1d6bb439
BrunoMatheus96 <brunomatheusstefanoleite@gmail.com>
2023-03-23 02:06:15 -0300

    Initial commit

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,58 @@
+package pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import runner.RunCucumber;
+
+public class LoginPage extends RunCucumber {
+
+    // elementos
+    private String URL = "http://automationpratice.com.br/";
+    private By botaoAcessarLogin = By.cssSelector(".right_list_fix > li > a > .fa-user");
+    private By campoEmail = By.id("user");
+    private By campoSenha = By.id("password");
+    private By botaoFazerLogin = By.id("btnLogin");
+    private By botaoAcessarCadastro = By.cssSelector(".right_list_fix > li > a > .fa-lock");
+
+    // ações / funções / métodos
+    public void acessarAplicao() {
+        getDriver("chrome").get(URL);
+    }
+
+    public void acessarTelaLogin() {
+        getDriver().findElement(botaoAcessarLogin).click();
+    }
+
+    public void preencheEmail(String email) {
+        getDriver().findElement(campoEmail).sendKeys(email);
+    }
+
+    public void preencherSenha(String senha) {
+        getDriver().findElement(campoSenha).sendKeys(senha);
+    }
+
+    public void clicarLogin() {
+        getDriver().findElement(botaoFazerLogin).click();
+    }
+
+    public void verificaLoginSucesso() {
+        String textoLoginSucesso = getDriver().findElement(By.id("swal2-title")).getText();
+        Assert.assertEquals("Login realizado", textoLoginSucesso);
+    }
+
+    public void verificaCampoVazio(String message) {
+        String textError = getDriver().findElement(By.className("invalid_input")).getText();
+        Assert.assertEquals(message, textError);
+    }
+
+    public void acessarTelaCadastro() {
+        getDriver().findElement(botaoAcessarCadastro).click();
+    }
+
+    public  void scrollDown(){
+        JavascriptExecutor jse = (JavascriptExecutor) getDriver();
+        jse.executeScript("window.scrollTo(0, 10000)");
+    }
+
+}
---
+++ src/test/java/steps/LoginSteps.java
@@ -0,0 +1,62 @@
+package steps;
+
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Quando;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import pages.LoginPage;
+import runner.RunCucumber;
+import org.openqa.selenium.JavascriptExecutor;
+
+import static java.lang.Thread.sleep;
+
+public class LoginSteps extends RunCucumber {
+
+    LoginPage loginPage = new LoginPage();
+
+    @Dado("^que estou na tela de login$")
+    public void que_estou_na_tela_de_login(){
+        loginPage.acessarAplicao();
+        loginPage.acessarTelaLogin();
+    }
+
+    @Quando("^preencho login \"([^\"]*)\" e senha \"([^\"]*)\"$")
+    public void preencho_login_e_senha(String email, String senha){
+        loginPage.preencheEmail(email);
+        loginPage.preencherSenha(senha);
+        loginPage.scrollDown();
+        getDriver().findElement(By.id("btnLogin")).sendKeys(Keys.TAB);
+    }
+
+    @Quando("^clico em Login$")
+    public void clico_em_Login() {
+        loginPage.clicarLogin();
+    }
+
+    @Então("^vejo mensagem de login com sucesso$")
+    public void vejo_mensagem_login_sucesso() {
+        loginPage.verificaLoginSucesso();
+    }
+
+    @Então("^vejo mensagem \"([^\"]*)\" de campo não preenchido$")
+    public void vejo_mensagem_de_campo_não_preenchido(String message) {
+        loginPage.verificaCampoVazio(message);
+    }
+
+    @Quando("^acesso a tela de cadastro de usuário$")
+    public void acesso_a_tela_de_cadastro_de_usuário() {
+        loginPage.acessarTelaCadastro();
+    }
+
+    @Dado("^que estou logado na aplicação com user \"([^\"]*)\" e senha \"([^\"]*)\"$")
+    public void que_estou_logado_na_aplicação_com_user_e_senha(String email, String senha) {
+        loginPage.acessarAplicao();
+        loginPage.acessarTelaLogin();
+        loginPage.preencheEmail(email);
+        loginPage.preencherSenha(senha);
+        loginPage.clicarLogin();
+        loginPage.verificaLoginSucesso();
+    }
+
+}
