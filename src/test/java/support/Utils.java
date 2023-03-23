ca58719ff33d5265e8574844cce4a73b3b0eb911
BrunoMatheus96 <brunomatheusstefanoleite@gmail.com>
2023-03-23 03:31:50 -0300

    criação de nova classe

--- src/test/java/support/Utils.java
+++ src/test/java/support/Utils.java
@@ -9,12 +9,6 @@ import runner.RunCucumber;
 import java.util.Random;
 
 public class Utils extends RunCucumber {
-
-    public static void waitElementBePresent(By element, Integer tempo) {
-        WebDriverWait wait = new WebDriverWait(getDriver(), tempo);
-        wait.until(ExpectedConditions.elementToBeClickable(element));
-    }
-
     public static String getRandomEmail() {
         String email_init = "qazando_";
         String email_final = "@qazando.com.br";
